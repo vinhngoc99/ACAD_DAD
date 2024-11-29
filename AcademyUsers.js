40de0cab846989af02b89f5c44b83a0608a0973e
vinhngoc99 <vinhnn.rtbed@gmail.com>
2024-11-29 13:22:20 +0700

    Add user

---
+++ AcademyUsers.js
@@ -0,0 +1,122 @@
+[
+    {
+        id: 1,
+        name: "Ngo Ngoc Vinh",
+        email: "vinhnn@pixelz.com",
+        account: "VinhNN",
+        password: "abc123*",
+        skill: {
+            fs: true,
+            shadow: true,
+            stencil: true,
+            dust: true,
+            wiethe: true,
+            ssense: true
+        }
+    },
+    {
+        id: 2,
+        name: "Le Hong Vuong",
+        email: "vuonglh@pixelz.com",
+        account: "VuongLH",
+        password: "abcd123*",
+        skill: {
+            fs: true,
+            shadow: true,
+            stencil: true,
+            dust: true,
+            wiethe: true,
+            ssense: true
+        }
+    },
+    {
+        id: 3,
+        name: "Trang Huyen Trang",
+        email: "trangth@pixelz.com",
+        account: "TrangTH",
+        password: "abcd123*",
+        skill: {
+            fs: true,
+            shadow: true,
+            stencil: true,
+            dust: true,
+            wiethe: true,
+            ssense: true
+        }
+    },
+    {
+        id: 4,
+        name: "Bui Thi Kim Anh",
+        email: "anhbtk@pixelz.com",
+        account: "AnhBTK",
+        password: "abcd123*",
+        skill: {
+            fs: true,
+            shadow: true,
+            stencil: true,
+            dust: true,
+            wiethe: true,
+            ssense: true
+        }
+    },
+    {
+        id: 5,
+        name: "Le Thi Phuong Thao",
+        email: "thaoltp@pixelz.com",
+        account: "ThaoLTP",
+        password: "abcd123*",
+        skill: {
+            fs: true,
+            shadow: true,
+            stencil: true,
+            dust: true,
+            wiethe: true,
+            ssense: true
+        }
+    },
+    {
+        id: 6,
+        name: "Tran Thi Ha",
+        email: "hatt@pixelz.com",
+        account: "HaTT",
+        password: "abcd123*",
+        skill: {
+            fs: true,
+            shadow: true,
+            stencil: true,
+            dust: true,
+            wiethe: true,
+            ssense: true
+        }
+    },
+    {
+        id: 7,
+        name: "Tran Thi Dung",
+        email: "dungtt@pixelz.com",
+        account: "DungTT",
+        password: "abcd123*",
+        skill: {
+            fs: true,
+            shadow: true,
+            stencil: true,
+            dust: true,
+            wiethe: true,
+            ssense: true
+        }
+    },
+    {
+        id: 8,
+        name: "Nong Duc Luan",
+        email: "luannd@pixelz.com",
+        account: "LuanND",
+        password: "abcd123*",
+        skill: {
+            fs: true,
+            shadow: true,
+            stencil: true,
+            dust: true,
+            wiethe: true,
+            ssense: true
+        }
+    }
+]
